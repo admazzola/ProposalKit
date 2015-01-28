d15639132b7583b7c129c18e3c42c40856677646
Andrew Mazzola <admazzola@gmail.com>
2015-01-28 15:41:43 -0500

    Added file input and output

--- js/proposalkit.js
+++ js/proposalkit.js
@@ -1,21 +1,37 @@
 
+var accountText;
+
    //var pageDocument = pageWindow.document;
    (function(view) {
 
+
+
+
    $( document ).ready(function() {
 
-     var outputfilename = $('#output-file-name-field');
+     var saveoutputform = document.getElementById("save-output-file");
+     var outputfilename = document.getElementById("output-file-name-field");
+     var accountfile = document.getElementById("accountfile");
+
+     accountfile.addEventListener('change', function(e) {
+          var file = this.files[0];
+          var reader = new FileReader();
+          reader.onload = function(e) {
+            accountText = reader.result; //this is the var with the stored data!
+          }
+            reader.readAsText(file);
+     }, false);
 
     var get_blob = function() {
        return view.Blob;
      }
 
-     document.getElementById("save-output-file").addEventListener("submit", function(event) {
+     saveoutputform.addEventListener("submit", function(event) {
   event.preventDefault();
   var BB = get_blob();
   saveAs(
       new BB(
-        ["this is a test - put the text in here!"]
+        [getOutputFileData() ]
       , {type: "text/plain;charset=" + document.characterSet}
     )
     , (outputfilename.value || outputfilename.placeholder) + ".txt" //the extension goes here!
@@ -24,4 +40,16 @@
 
 
 });
+
+
+
+
+
 }(self));
+
+function getOutputFileData()
+{
+  //do number crunching here
+  return "this is a test - put the text in here! " + accountText;
+
+}
